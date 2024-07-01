c511f5289c2c18a0f31fedce3e2ab516923c6617
James <apisitamornktn@gmail.com>
2024-07-01 17:46:47 +0700

    feat:add validate token

--- client/src/utils/jwtInterceptor.js
+++ client/src/utils/jwtInterceptor.js
@@ -6,7 +6,14 @@ function jwtInterceptor() {
     //  ให้เขียน Logic ในการแนบ Token เข้าไปใน Header ของ Request
     // เมื่อมีการส่ง Request จาก Client ไปหา Server
     // ภายใน Callback Function axios.interceptors.request.use
+    const hasToken = Boolean(window.localStorage.getItem("token"));
 
+    if (hasToken) {
+      req.headers = {
+        ...req.headers,
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+      };
+    }
     return req;
   });
 
@@ -19,7 +26,13 @@ function jwtInterceptor() {
       //  ให้เขียน Logic ในการรองรับเมื่อ Server ได้ Response กลับมาเป็น Error
       // โดยการ Redirect ผู้ใช้งานไปที่หน้า Login และลบ Token ออกจาก Local Storage
       // ภายใน Error Callback Function ของ axios.interceptors.response.use
-
+      if (
+        error.response.status === 401 &&
+        error.response.statusText === "Unauthorized"
+      ) {
+        window.localStorage.removeItem("token");
+        window.location.replace("/login");
+      }
       return Promise.reject(error);
     }
   );
